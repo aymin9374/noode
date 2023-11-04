631f93e1151a8ba1633ba3b721c617d54dfbe3c6
khaled helmy <khaledhelmyg@gmail.com>
2023-11-04 20:36:56 +0200

    add database azure config

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+const fs = require("fs");
 const { connect } = require("pm2");
 
 var config = {
@@ -9,15 +10,33 @@ var config = {
   connectionLimit: 10,
   charset: "utf8mb4",
 };
-
-// var conn = mysql.createPool(config);
-var conn = mysql.createPool(config, function (err) {
-  if (err) {
-    console.log(err);
-    console.error("Error connecting to database: " + err.stack);
-    return;
+var conn = mysql.createConnection(
+  {
+    host: "chatt.mysql.database.azure.com",
+    user: "khaled",
+    password: "chat12345@@",
+    database: "chatdb_v2",
+    options: { ReturnSingleDatabase: true },
+    port: 3306,
+    ssl: { ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem") },
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+      console.error("Error connecting to database: " + err.stack);
+      return;
+    }
+    console.log("Connected to database with threadId: " + conn.threadId);
   }
-  console.log("Connected to database with threadId: " + conn.threadId);
-});
+);
+// var conn = mysql.createPool(config);
+// var conn = mysql.createPool(config, function (err) {
+//   if (err) {
+//     console.log(err);
+//     console.error("Error connecting to database: " + err.stack);
+//     return;
+//   }
+//   console.log("Connected to database with threadId: " + conn.threadId);
+// });
 
 module.exports = { conn };
